0f45fe866d9e45d0dd0f153add7743202fec4cc3
rakshitha818 <131687355+rakshitha818@users.noreply.github.com>
2023-11-18 12:56:02 +0530

    Test of Testvagrant
    Rakshitha S

---
+++ #Rakshitha S.py
@@ -0,0 +1,27 @@
+#Rakshitha S
+#BangaloreInstituteofTechnology
+#2024
+#8310759870
+#srakshithaa08@gmail.com
+
+product={'leather','umbrella','cigarette','honey'}
+leather=1100
+umbrella=900
+cigarette=200
+honey=100
+gst=0
+ 
+if product==leather:
+    gst=leather+((18/1100)*100)
+    quantity=1*leather
+elif product==umbrella:
+    gst=umbrella+((12/900)*100)
+    quantity=4*umbrella
+elif product==cigarette:
+    gst=cigarette+((28/200)*100)
+    quantity=3*cigarette   
+else:
+    gst=honey
+    quantity=2*honey
+
+
